9db9b47593100ab1f84aa1c1150c46319c4a85b6
Luiz Wynne <luiz.wynne@casar.com>
2023-04-05 15:02:53 -0300

    Lab B finished

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,5 +1,6 @@
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
+import ProjectForm from './ProjectForm';
 
 interface ProjectListProps {
   projects: Project[];
@@ -12,6 +13,7 @@ function ProjectList({ projects }: ProjectListProps) {
      {projects.map((project) => (
         <div key={project.id} className="cols-sm">
           <ProjectCard project={project}></ProjectCard>
+          <ProjectForm />
         </div>
       ))}
     </div>
