6f01c6ff25df84f98fcfefea03a169827a320541
Luiz Wynne <luiz.wynne@casar.com>
2023-04-05 14:32:15 -0300

    first commit

---
+++ src/projects/ProjectList.tsx
@@ -0,0 +1,21 @@
+import { Project } from './Project';
+import ProjectCard from './ProjectCard';
+
+interface ProjectListProps {
+  projects: Project[];
+}
+
+
+function ProjectList({ projects }: ProjectListProps) {
+  return (
+    <div className="row">
+     {projects.map((project) => (
+        <div key={project.id} className="cols-sm">
+          <ProjectCard project={project}></ProjectCard>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ProjectList;
