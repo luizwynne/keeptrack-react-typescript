965629458fb3f2ee0ab2e88e098b1b76f40e437e
Luiz Wynne <luiz.wynne@casar.com>
2023-04-12 20:04:01 -0300

    Made it to lab 17

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -5,10 +5,11 @@ import ProjectForm from './ProjectForm';
 
 interface ProjectListProps {
   projects: Project[];
+  onSave: (project: Project) => void;
 }
 
 
-function ProjectList({ projects }: ProjectListProps) {
+function ProjectList({ projects, onSave }: ProjectListProps) {
 
   const [projectBeingEdited, setProjectBeingEdited] = useState({});
 
@@ -17,12 +18,16 @@ function ProjectList({ projects }: ProjectListProps) {
     setProjectBeingEdited(project);
   };
 
+  const cancelEditing = () => {
+       setProjectBeingEdited({});
+  };
+
   return (
     <div className="row">
      {projects.map((project) => (
         <div key={project.id} className="cols-sm">
           {project === projectBeingEdited ? (
-            <ProjectForm />
+            <ProjectForm project={project} onSave={onSave} onCancel={cancelEditing} />
           ) : (
             <ProjectCard project={project} onEdit={handleEdit} />
           )}
